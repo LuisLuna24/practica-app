64956380826180b0ce12e9a9e2ddacf740945214
LuisLuna24 <eduarlun4@gmail.com>
2024-03-11 15:43:14 -0600

    Configuras

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Idea;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -29,4 +30,6 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
+
+    
 };
